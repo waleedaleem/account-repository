210a73e2455999bdb46efca8895ed79d0d5c8cc2
Walid Moustafa <waleedaleem@hotmail.com>
2020-10-09 20:40:10 +1100

    Initial commit

---
+++ src/test/java/com/walid/AccountListApplicationTests.java
@@ -0,0 +1,11 @@
+package com.walid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class AccountListApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
